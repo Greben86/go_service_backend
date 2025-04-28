3382c9ab2629d2770f3ffbf96e028bcead243783
Viktor <grebenvictor@yandex.ru>
2025-04-29 00:02:47 +0300

    Добавил авторизацию

--- src/rest_module/api.go
+++ src/rest_module/api.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -15,46 +18,124 @@ var JWT_SECRET = "53A73E5F1C4E0A2D3B5F2D784E6A1B423D6F247D1F6E5C3A596D635A753278
 
 // API приложения.
 type API struct {
-	r *mux.Router // маршрутизатор запросов
-	// db *db.DB     // база данных
+	r  *mux.Router // маршрутизатор запросов
+	db *DB         // база данных
 }
 
 // Конструктор API.
-func ApiNewInstance() *API {
+func ApiNewInstance(db *DB) *API {
 	api := API{}
-	// api.db = db
+	api.db = db
 	api.r = mux.NewRouter()
 	api.endpoints()
 	return &api
 }
 
 func (api *API) exampleHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("example")
-	w.Write([]byte("example"))
+	fmt.Println("Authorization: " + r.Header.Values("Authorization")[0])
+	fmt.Println("ID: " + fmt.Sprintf("%#v", r.Context().Value("id")))
+	id, _ := strconv.Atoi(fmt.Sprintf("%#v", r.Context().Value("id")))
+	user := api.db.GetUser(id)
+	fmt.Println(id)
+
+	w.Write([]byte(user.Username))
 }
 
 func (api *API) registerHandler(w http.ResponseWriter, r *http.Request) {
-	var token, _ = GenerateJWTToken("1")
+	// Читаем тело запроса с помощью io.ReadAll
+	body, err := io.ReadAll(r.Body)
+
+	// Закрываем тело запроса
+	defer r.Body.Close()
+
+	// Проверяем наличие ошибок
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Выводим тело запроса в ответ
+	request := RequestSignUp{}
+	err = json.Unmarshal(body, &request)
+
+	// Проверяем наличие ошибок
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user := User{Username: request.Username, Password: request.Password}
+	user.ID = api.db.NewUser(user)
+
+	var token, _ = GenerateJWTToken(fmt.Sprint(user.ID))
+
+	fmt.Println(request)
+	var responseDTO = ResponseAuth{JwtToken: token}
+
+	response, _ := json.Marshal(&responseDTO)
+	w.Write(response)
+
 	fmt.Println("register")
-	w.Write([]byte("token: " + token))
 }
 
 func (api *API) loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Читаем тело запроса с помощью io.ReadAll
+	body, err := io.ReadAll(r.Body)
+
+	// Закрываем тело запроса
+	defer r.Body.Close()
+
+	// Проверяем наличие ошибок
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Выводим тело запроса в ответ
+	request := RequestSignUp{}
+	err = json.Unmarshal([]byte(body), &request)
+
+	// Проверяем наличие ошибок
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var users []User = api.db.Users()
+	var user User
+	for i := 0; i < len(users); i++ {
+		element := users[i]
+		if element.Username == request.Username && element.Password == request.Password {
+			user = element
+			break
+		}
+	}
+
+	var token, _ = GenerateJWTToken(string(user.ID))
+	var responseDTO = ResponseAuth{JwtToken: token}
+
+	response, _ := json.Marshal(&responseDTO)
+	w.Write(response)
+
 	fmt.Println("login")
-	w.Write([]byte("login"))
 }
 
 // Регистрация методов API в маршрутизаторе запросов.
 func (api *API) endpoints() {
 	// Предшествующий код
 	// Public routes
-	api.r.HandleFunc("/sign/up", api.registerHandler).Methods(http.MethodPost)
-	api.r.HandleFunc("/sign/in", api.loginHandler).Methods(http.MethodPost)
+	api.Router().HandleFunc("/register", api.registerHandler).Methods(http.MethodPost) // регистрация
+	api.Router().HandleFunc("/login", api.loginHandler).Methods(http.MethodPost)       // аутентификация
 	// Protected routes
-	authRouter := api.r.PathPrefix("/").Subrouter()
+	authRouter := api.Router().PathPrefix("/").Subrouter()
 	authRouter.Use(AuthMiddleware)
 
-	authRouter.HandleFunc("/example", api.exampleHandler).Methods(http.MethodGet)
+	authRouter.HandleFunc("/accounts", api.exampleHandler).Methods(http.MethodPost)                    // создать счет
+	authRouter.HandleFunc("/cards", api.exampleHandler).Methods(http.MethodPost)                       // выпустить карту
+	authRouter.HandleFunc("/transfer", api.exampleHandler).Methods(http.MethodPost)                    // перевод средств
+	authRouter.HandleFunc("/analytics", api.exampleHandler).Methods(http.MethodGet)                    // получить аналитику
+	authRouter.HandleFunc("/credits/{creditId}/schedule", api.exampleHandler).Methods(http.MethodGet)  // график платежей по кредиту
+	authRouter.HandleFunc("/accounts/{accountId}/predict", api.exampleHandler).Methods(http.MethodGet) // прогноз баланса
 }
 
 // Router возвращает маршрутизатор запросов.
--- src/rest_module/main.go
+++ src/rest_module/main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
+	// Инициализация БД в памяти.
+	db := NewDB()
 	// Создание объекта API, использующего БД в памяти.
-	api := ApiNewInstance()
+	api := ApiNewInstance(db)
 	// Запуск сетевой службы и HTTP-сервера
 	// на всех локальных IP-адресах на порту 8081.
 	err := http.ListenAndServe(":8081", api.Router())
